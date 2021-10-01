f5f39e0a93020af959e172e2a9a2a2c75c281f1f
Michelle Foucher <michelle.foucher@gmail.com>
2021-10-01 11:43:45 -0300

    Segunda entrega Proyecto Final

--- javascript/app.js
+++ javascript/app.js
@@ -4,27 +4,59 @@ const typeName = document.querySelectorAll(".enlaces-price__type");
 const value = document.querySelector("#value");
 
 
-console.log(value);
-
 buttonWholesaler.addEventListener("click", () => {
     for(let type of typeName) {
         type.textContent = "/Mayorista";
     }
-    value.textContent = "$250";
+    value1.textContent = "$250";
+    value2.textContent = "$250";
+    value3.textContent = "$250";
     buttonWholesaler.classList.remove("button-unchoosed");
     buttonWholesaler.classList.add("button-choosed");
     buttonRetailer.classList.remove("button-choosed");
     buttonRetailer.classList.add("button-unchoosed");
+
+    localStorage.setItem('buyClass','Mayorista');
 });
 
 buttonRetailer.addEventListener("click", () => {
     for(let type of typeName) {
         type.textContent = "/Minorista";
     }
-    value.textContent = "$300";
+    value1.textContent = "$300";
+    value2.textContent = "$300";
+    value3.textContent = "$300";
     buttonRetailer.classList.remove("button-unchoosed");
     buttonRetailer.classList.add("button-choosed");
     buttonWholesaler.classList.remove("button-choosed");
     buttonWholesaler.classList.add("button-unchoosed");
+
+    localStorage.setItem('buyClass','Minorista');
 });
 
+window.addEventListener('DOMContentLoaded',() => {
+    if(localStorage.getItem('buyClass') === 'Minorista'){
+        for(let type of typeName) {
+            type.textContent = "/Minorista";
+        }
+        value1.textContent = "$300";
+        value2.textContent = "$300";
+        value3.textContent = "$300";
+        buttonRetailer.classList.remove("button-unchoosed");
+        buttonRetailer.classList.add("button-choosed");
+        buttonWholesaler.classList.remove("button-choosed");
+        buttonWholesaler.classList.add("button-unchoosed");
+    }else{
+        for(let type of typeName) {
+            type.textContent = "/Mayorista";
+        }
+        value1.textContent = "$250";
+        value2.textContent = "$250";
+        value3.textContent = "$250";
+        buttonWholesaler.classList.remove("button-unchoosed");
+        buttonWholesaler.classList.add("button-choosed");
+        buttonRetailer.classList.remove("button-choosed");
+        buttonRetailer.classList.add("button-unchoosed");
+    }
+  
+})
